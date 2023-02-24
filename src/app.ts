57b1d31c90db388404458db7868fa1151b769a63
`thais` <`tjlannes@gmail.com`>
2023-02-24 20:20:38 +0100

    create app and delete the your default initial test

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-export {};
+import express from 'express';
+import apiRouter from './api/api-router.js';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({ hello: 'World' });
+});
+app.use(express.json());
+app.use('/api/v1', apiRouter);
+
+export default app;
