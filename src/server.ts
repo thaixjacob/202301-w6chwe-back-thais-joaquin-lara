9d229c63a0d9357a76cfec310e63a6ff87b6efd7
JoaccoG <joaquingodoy2407@gmail.com>
2023-02-26 17:14:14 +0100

    Fix controllers reading mongo id instead of our generated uuid

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 import app from './app.js';
 import connectDB from './database/connection.js';
 import log from './logger.js';
+
 dotenv.config();
 
 const port = process.env.PORT ?? 3000;
 const mongoUrl = process.env.MONGO_URL ?? '';
+
+mongoose.set('strictQuery', false);
 
 app.listen(port, async () => {
   await connectDB(mongoUrl);
