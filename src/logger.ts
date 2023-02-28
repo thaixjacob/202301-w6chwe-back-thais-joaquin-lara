fc383155864b5fa8ac943cad449356da638953c5
`thais` <`tjlannes@gmail.com`>
2023-02-28 20:53:17 +0100

    import dotenv module

--- src/logger.ts
+++ src/logger.ts
@@ -1,4 +1,7 @@
 import bunyan, { LogLevel } from 'bunyan';
+import dotenv from 'dotenv';
+dotenv.config();
+
 const log = bunyan.createLogger({ name: 'DemoApp' });
 log.level((process.env.BUNYAN_LEVEL as LogLevel) ?? 'info');
 
