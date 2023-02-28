f7d300e743434c781bd114e0fcfde0de768474d6
`thais` <`tjlannes@gmail.com`>
2023-02-28 20:52:47 +0100

    import router auth

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import apiRouter from './api/api-router.js';
 import cors from 'cors';
+import authRouter from './api/auth/auth-router.js';
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.get('/', (_req, res) => {
 app.use(cors());
 app.use(express.json());
 app.use('/api/v1', apiRouter);
+app.use('/auth', authRouter);
 app.disable('x-powered-by');
 
 export default app;
