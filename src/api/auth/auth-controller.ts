b6383d74f190e3f0b61f23e984ea5eab5079e710
`thais` <`tjlannes@gmail.com`>
2023-02-28 20:10:09 +0100

    create authentication of the user

---
+++ src/api/auth/auth-controller.ts
@@ -0,0 +1,85 @@
+import { RequestHandler } from 'express';
+import logger from '../../logger';
+import crypto from 'node:crypto';
+import { User, UserModel } from '../user/user-schema';
+import { encryptPassword, generateJWTToken } from './auth-utils.js';
+
+const EMAIL_REGEX_VALIDATION = /^[a-z0-9_.+-]+@[a-z0-9-]+\.[a-z0-9-.]+$/i;
+
+const PASSWORD_REGEX_VALIDATION =
+  /^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{6}$/;
+
+interface LoginResponse {
+  accessToken: string;
+}
+
+type AuthRequest = Pick<User, 'email' | 'password'>;
+
+export const registerUserController: RequestHandler<
+  unknown,
+  unknown,
+  AuthRequest
+> = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!EMAIL_REGEX_VALIDATION.test(email)) {
+    return res.status(400).json({ msg: 'Email must be a valid email.' });
+  }
+
+  if (!PASSWORD_REGEX_VALIDATION.test(password)) {
+    return res.status(400).json({
+      msg: 'Your password must contain one capital letter, one number and one special character number.',
+    });
+  }
+
+  if (!password) {
+    return res.status(400).json({ msg: 'Password must be defined.' });
+  }
+
+  try {
+    const userExistsDbUser = await UserModel.findOne({ email }).exec();
+
+    if (userExistsDbUser !== null) {
+      return res.status(409).json({ msg: 'User already registered' });
+    }
+
+    const user = {
+      id: crypto.randomUUID(),
+      email,
+      password: encryptPassword(password),
+    };
+
+    await UserModel.create(user);
+    res.sendStatus(201);
+  } catch (err) {
+    logger.error('Error registering user', err);
+    res.sendStatus(500);
+  }
+};
+
+export const loginUserController: RequestHandler<
+  unknown,
+  LoginResponse,
+  AuthRequest
+> = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const filterUser = {
+      email,
+      password: encryptPassword(password),
+    };
+
+    const existingUser = await UserModel.find(filterUser).exec();
+
+    if (existingUser === null) {
+      return res.sendStatus(404);
+    }
+
+    const tokenJWT = generateJWTToken(email);
+    res.status(201).json({
+      accessToken: tokenJWT,
+    });
+  } catch {
+    res.sendStatus(500);
+  }
+};
